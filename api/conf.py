cd59f85296fcfdf13f2b82e411a9fec4848fde5f
Luke Raeside <lukeraeside@lukes-mbp.home>
2026-04-01 14:38:47 +0100

    changed mongo to auth and modified JSON in conf.py to include username and password

--- api/conf.py
+++ api/conf.py
@@ -1,8 +1,11 @@
 PORT = 4000
-
+#Added username and password to authenticated db login
+#This will use keyring also
 MONGODB_HOST = {
     'host': 'localhost',
-    'port': 27017
+    'port': 27017,
+    'username': "myUserAdmin",
+    'password': "admindb"
 }
 
 MONGODB_DBNAME = 'cyberStudents'
