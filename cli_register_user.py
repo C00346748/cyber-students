66f0873097fce12bfc5c79f68d2d03cadd43ffa4
Luke Raeside <lukeraeside@lukes-mbp.home>
2026-04-11 17:56:02 +0100

    Stabilising the input tests and pwinput added

--- cli_register_user.py
+++ cli_register_user.py
@@ -13,31 +13,33 @@ import keyring
 import aes_encrypt_decrypt
 import crypto_helper
 import asyncio
+import tracemalloc
+import pwinput
 
 from api.handlers.registration import RegistrationHandler
 
 import urllib.parse
 
 
-#A limited amount of input, this time not using command line
-async def limited_data():
-    email = input('Please enter your email')
-    password = input('Enter your password: ')
-    displayName = input('Enter the display name to use')
-    crypto_helper.set_salt(email)
-    registration(email,password,displayName)
+
 
 #Sends reg request to webserver
-async def registration(email_in,password_in,display_name_in):
+async def cli_registration():
+    tracemalloc.start()
+    print(f"*** Calling reg ***:")
+    email_in = input('Please enter your email: ')
+    password_in = pwinput.pwinput('Enter your password: ')
+    display_name_in = input('Enter the display name to use: ')
+
     my_app = Application([(r'/registration', RegistrationHandler)])
-    print(f"The app is:  {my_app}")
+    
     my_app.db = AsyncMongoMockClient()[MONGODB_DBNAME]
     my_app.executor = ThreadPoolExecutor(WORKERS)
     
     email = crypto_helper.encrypt_email(email_in)
     display_name = crypto_helper.encrypt_other_string(email_in,display_name_in)
     #print(f"Salt and peppered display name {display_name}")
-    password = crypto_helper.encrypt_pwd(email_in)
+    password = crypto_helper.encrypt_pwd_new(password_in,email_in)
     #print(f"**** Password and Email Reg ****  {email} password {password}")
     #body dictionary replacing password retrieval with keyring
     body = {
@@ -58,15 +60,7 @@ async def registration(email_in,password_in,display_name_in):
         headers={"Content-Type": "application/json"}
     )
     print(f"Server response {response.code}")
-
-    #response2 = await my_app.db.users.insert_one({
-           # 'email': email,
-           # 'password': password,
-           # 'displayName': display_name
-    #})
     
-    #print("Server response " + response2.code)
-
     #Configure to run as script
 if __name__ == '__main__':
-    asyncio.run(limited_data())
+    asyncio.run(cli_registration())
